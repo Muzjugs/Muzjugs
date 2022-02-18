9a721f63a57419b333abbd102b956002c739dec8
Muzjugs <99413955+Muzjugs@users.noreply.github.com>
2022-02-18 15:37:24 +0300

    Create Chickens!.java

---
+++ Chickens!.java
@@ -0,0 +1,30 @@
+class Main {
+  public static void main(String[] args) {
+    int totalEggs = 15;
+     int eggsPerChicken = 4;
+     int chickenCount = 8;
+     totalEggs= eggsPerChicken*chickenCount;
+     //monday
+     System.out.println("Monday:"+totalEggs);
+     //tuesday
+     chickenCount++;
+     totalEggs+= eggsPerChicken*chickenCount;
+     System.out.println("Tuesday:"+totalEggs);
+     //wednesday
+     totalEggs+= eggsPerChicken*chickenCount;
+     System.out.println("Wednesday:"+totalEggs);
+     //day until sunday
+     chickenCount=chickenCount/2;
+      totalEggs+= eggsPerChicken*chickenCount;
+      System.out.println("Thursday:"+totalEggs);
+      totalEggs+= eggsPerChicken*chickenCount;
+      System.out.println("Friday:"+totalEggs);
+     totalEggs+= eggsPerChicken*chickenCount;
+      System.out.println("Saturday:"+totalEggs);
+     totalEggs+= eggsPerChicken*chickenCount;
+      System.out.println("Sunday:"+totalEggs);
+  
+ 
+
+  }
+}
